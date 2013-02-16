24e1ec33611dc9e45ccd86af576d5482109e0580
graygray <llkkqq@gmail.com>
2013-02-16 23:13:46 +0800

    +crc

--- Makefile
+++ Makefile
@@ -3,10 +3,12 @@
 CC = g++
 CFLAGS = -ansi -O -Wall
 PRJ_PATH = D:\prj\github\test
-all: test.o
-	${CC} $(CFLAGS) -o x test.o 
+all: test.o lib_crc.o
+	${CC} $(CFLAGS) -o x test.o lib_crc.o
 test.o: 
 	${CC} $(CFLAGS) test.cpp -c
+lib_crc.o: 
+	${CC} $(CFLAGS) lib_crc.c -c
 .PHONY : clean 
 clean:
 	rm -rf *.o *.exe
--- test.cpp
+++ test.cpp
@@ -3,10 +3,12 @@
 #include<vector>
 #include<algorithm>
 #include<assert.h>
+#include "lib_crc.h"
 
 using namespace std; 
 
 #define MIN(a,b) (a>b) ? b : a
+#define MAX_STRING_SIZE	2048
 
 void default_func_test(int val=2){
 
@@ -14,45 +16,106 @@ void default_func_test(int val=2){
 	printf(" val=%d\n", val);
 }
 
-unsigned char crc(
-		unsigned int data, unsigned int data_len,
+//#define POLY 0x8408
+#define POLY 0x8408
+unsigned short crc16(unsigned char *data_p, unsigned short length)
+{
+      unsigned char i;
+      unsigned int data;
+      unsigned int crc = 0xffff;
+
+      if (length == 0)
+            return (~crc);
+
+      do
+      {
+            for (i=0, data=(unsigned int)0xff & *data_p++;
+                 i < 8;
+                 i++, data >>= 1)
+            {
+                  if ((crc & 0x0001) ^ (data & 0x0001))
+                        crc = (crc >> 1) ^ POLY;
+                  else  crc >>= 1;
+            }
+      } while (--length);
+
+      crc = ~crc;
+      data = crc;
+      crc = (crc << 8) | (data >> 8 & 0xff);
+
+      return (crc);
+}
+
+uint32_t crc_table[256];
+/* Run this function previously */
+void make_crc_table(void) {
+    for (uint32_t i = 0; i < 256; i++) {
+        uint32_t c = i;
+        for (int j = 0; j < 8; j++) {
+            c = (c & 1) ? (0xEDB88320 ^ (c >> 1)) : (c >> 1);
+        }
+        crc_table[i] = c;
+    }
+}
+
+uint32_t crc32(uint8_t *buf, size_t len) {
+    uint32_t c = 0xFFFFFFFF;
+    for (size_t i = 0; i < len; i++) {
+        c = crc_table[(c ^ buf[i]) & 0xFF] ^ (c >> 8);
+    }
+    return c ^ 0xFFFFFFFF;
+}
+
+//#define CRC32Const 0x04C11DB7
+#define CRC32Const 0xEDB88320
+//#define CRC32Const 0x82608EDB
+unsigned int crc(
+		unsigned char* data, unsigned int data_len,
 		unsigned int gen, unsigned int gen_len) {
 
-	unsigned int res;
-	unsigned int temp;
-	int i;
+	unsigned int crc = 0;
 
-	printf("[gray] test.cpp:%s:%d, data:0x%X\n", __FUNCTION__, __LINE__, data);
+//	crc = data[0] << 24;
+//	printf("[gray] test.cpp:%s:%d, crc:0x%X\n", __FUNCTION__, __LINE__, crc);
+//	crc = crc | (data[1] << 16);
+//	printf("[gray] test.cpp:%s:%d, crc:0x%X\n", __FUNCTION__, __LINE__, crc);
+//	crc = crc | (data[2] << 8);
+//	printf("[gray] test.cpp:%s:%d, crc:0x%X\n", __FUNCTION__, __LINE__, crc);
+//	crc = crc | ( data[3] );
+//	printf("[gray] test.cpp:%s:%d, crc:0x%X\n", __FUNCTION__, __LINE__, crc);
 
-	data = data << gen_len;
-	printf("[gray] test.cpp:%s:%d, data:0x%X\n", __FUNCTION__, __LINE__, data);
+	int i, j;
+	int var;
+	for (i = 0; i < data_len; ++i)
+	{
 
-	temp = data >> data_len;
-	printf("[gray] test.cpp:%s:%d, temp:0x%X\n", __FUNCTION__, __LINE__, temp);
-	temp = temp ^ gen;
+		// count in a byte
+		for (j = 0; j < 8; ++j)
+		{
 
-	for (i = 0; i < data_len-1; ++i) {
+			if (crc & 0x80000000)
+			{
+				crc = crc ^ CRC32Const;
+			}
+			crc = crc << 1;
+			crc = crc | (data[i] >> (7 - j) & 1);
 
-		printf("[gray] test.cpp:%s:%d, temp:0x%X\n", __FUNCTION__, __LINE__, temp);
-		temp = temp << 1;
-		printf("[gray] test.cpp:%s:%d, temp:0x%X\n", __FUNCTION__, __LINE__, temp);
-//		printf("[gray] test.cpp:%s:%d, --0x%X\n", __FUNCTION__, __LINE__, ( (data >> (data_len-(i+1))) & 1));
-		temp = temp | ( (data >> (data_len-(i+1))) & 1);
-		printf("[gray] test.cpp:%s:%d, temp:0x%X\n", __FUNCTION__, __LINE__, temp);
-
-		if (temp >> gen_len-1 & 1) {
-
-			printf("[gray] 1\n");
-			temp = temp ^ gen;
-
-		} else {
-			printf("[gray] 0\n");
-			continue;
 		}
-
+	printf("[gray] test.cpp:%s:%d, crc:0x%X, i:%d\n", __FUNCTION__, __LINE__, crc, i);
 	}
-	res = temp;
-	return res;
+
+	for (var = 0; var < 31; ++var)
+	{
+
+		if (crc & 0x80000000)
+		{
+			crc = crc ^ CRC32Const;
+		}
+		crc = crc << 1;
+	}
+
+	printf("[gray] test.cpp:%s:%d, crc:0x%X, i:%d\n", __FUNCTION__, __LINE__, crc, i);
+	return crc;
 
 }
 
@@ -119,8 +182,13 @@ int main(int argc, char *argv[])
 
 		printf("[gray] test.cpp:%s:%d\n", __FUNCTION__, __LINE__);
 		FILE *fp;
+		unsigned char buf[512];
 
+		memset(buf, 0, sizeof(buf));
+
+		fp = fopen("Y0199.bin", "rb");
 		fp = fopen("T2051.bin", "rb");
+		fp = fopen("C0877.bin", "rb");
 		if (!fp) {
 			printf("[gray] test.cpp:%s:%d, open fail\n", __FUNCTION__, __LINE__);
 			fclose(fp);
@@ -131,8 +199,9 @@ int main(int argc, char *argv[])
 		c = fgetc(fp);
 		while(!feof(fp))
 		{
-			i++;
 			printf("%02X ", c);
+			buf[i] = c;
+			i++;
 			c = fgetc(fp);
 		}
 		printf("\n");
@@ -141,11 +210,101 @@ int main(int argc, char *argv[])
 
 		fclose(fp);
 
+		printf("[gray] test.cpp:%s:%d, mycrc:0x%X\n", __FUNCTION__, __LINE__, crc(buf, i, 0x04C11DB7 , 32));
+//		printf("[gray] test.cpp:%s:%d, crc16:0x%X\n", __FUNCTION__, __LINE__, crc16(buf, i));
+		printf("[gray] test.cpp:%s:%d, crc16:0x%X\n", __FUNCTION__, __LINE__, crc16(buf, i));
+
+		make_crc_table();
+		printf("[gray] test.cpp:%s:%d, crc32:0x%X\n", __FUNCTION__, __LINE__, crc32(buf, i));
+
+
 	// CRC
 	} else if (!strcmp(argv[1], "crc")) {
 
 		printf("[gray] test.cpp:%s:%d\n", __FUNCTION__, __LINE__);
-		printf("[gray] test.cpp:%s:%d, crc:0x%X\n", __FUNCTION__, __LINE__, crc(0x35B, 10, 0x13, 5));
+
+	    char input_string[MAX_STRING_SIZE];
+	    char *ptr, *dest, hex_val, prev_byte;
+	    unsigned short crc_16, crc_16_modbus, crc_ccitt_ffff, crc_ccitt_0000, crc_ccitt_1d0f, crc_dnp, crc_sick, crc_kermit;
+	    unsigned short low_byte, high_byte;
+	    unsigned long crc_32;
+	    int a, ch, do_ascii, do_hex;
+	    FILE *fp;
+
+	    do_ascii = FALSE;
+	    do_hex   = FALSE;
+
+
+	    crc_16 = 0;
+		crc_16_modbus = 0xffff;
+		crc_dnp = 0;
+		crc_sick = 0;
+		crc_ccitt_0000 = 0;
+		crc_ccitt_ffff = 0xffff;
+		crc_ccitt_1d0f = 0x1d0f;
+		crc_kermit = 0;
+		crc_32 = 0xffffffffL;
+
+
+		prev_byte = 0;
+		fp = fopen(argv[2], "rb");
+
+		if (fp != NULL)
+		{
+
+			while ((ch = fgetc(fp)) != EOF)
+			{
+
+				crc_16 = update_crc_16(crc_16, (char) ch);
+				crc_16_modbus = update_crc_16(crc_16_modbus, (char) ch);
+				crc_dnp = update_crc_dnp(crc_dnp, (char) ch);
+				crc_sick = update_crc_sick(crc_sick, (char) ch, prev_byte);
+				crc_ccitt_0000 = update_crc_ccitt(crc_ccitt_0000, (char) ch);
+				crc_ccitt_ffff = update_crc_ccitt(crc_ccitt_ffff, (char) ch);
+				crc_ccitt_1d0f = update_crc_ccitt(crc_ccitt_1d0f, (char) ch);
+				crc_kermit = update_crc_kermit(crc_kermit, (char) ch);
+				crc_32 = update_crc_32(crc_32, (char) ch);
+
+				prev_byte = (char) ch;
+			}
+
+			fclose(fp);
+		}
+		else
+			printf("%s : cannot open file\n", argv[2]);
+
+
+	   crc_32 ^= 0xffffffffL;
+
+		crc_dnp = ~crc_dnp;
+		low_byte = (crc_dnp & 0xff00) >> 8;
+		high_byte = (crc_dnp & 0x00ff) << 8;
+		crc_dnp = low_byte | high_byte;
+
+		low_byte = (crc_sick & 0xff00) >> 8;
+		high_byte = (crc_sick & 0x00ff) << 8;
+		crc_sick = low_byte | high_byte;
+
+		low_byte = (crc_kermit & 0xff00) >> 8;
+		high_byte = (crc_kermit & 0x00ff) << 8;
+		crc_kermit = low_byte | high_byte;
+
+		printf("%s%s%s :\nCRC16              = 0x%04X      /  %u\n"
+				"CRC16 (Modbus)     = 0x%04X      /  %u\n"
+				"CRC16 (Sick)       = 0x%04X      /  %u\n"
+				"CRC-CCITT (0x0000) = 0x%04X      /  %u\n"
+				"CRC-CCITT (0xffff) = 0x%04X      /  %u\n"
+				"CRC-CCITT (0x1d0f) = 0x%04X      /  %u\n"
+				"CRC-CCITT (Kermit) = 0x%04X      /  %u\n"
+				"CRC-DNP            = 0x%04X      /  %u\n"
+				"CRC32              = 0x%08lX  /  %lu\n",
+				(do_ascii || do_hex) ? "\"" : "",
+				(!do_ascii && !do_hex) ? argv[2] : input_string,
+				(do_ascii || do_hex) ? "\"" : "", crc_16, crc_16, crc_16_modbus,
+				crc_16_modbus, crc_sick, crc_sick, crc_ccitt_0000,
+				crc_ccitt_0000, crc_ccitt_ffff, crc_ccitt_ffff, crc_ccitt_1d0f,
+				crc_ccitt_1d0f, crc_kermit, crc_kermit, crc_dnp, crc_dnp,
+				crc_32, crc_32);
 
 
 	} else {
